c7a85f59a51692d777338880f40d09c989adb70f
Mutlu Çalkan <mutlu.calkan@gmail.com>
2023-12-27 09:39:33 +0300

    Storyboard removed. Main folders created.

---
+++ Spotify/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotify
+//
+//  Created by Mutlu Çalkan on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home View"
+        view.backgroundColor = .red
+    }
+
+
+}
+
